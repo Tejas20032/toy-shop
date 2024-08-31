891c55adaa2d1813959f80299ebb828749ecf91d
Tejas HK <115473875+Tejas20032@users.noreply.github.com>
2024-08-31 16:02:05 +0530

    Delete js/Cart.js

--- js/Cart.js
+++
@@ -1,8 +0,0 @@
-function numgenerator() {
-    var num = Math.floor(Math.random() * 3000 + 500);
-    document.getElementById("display").value = num;
-}
-function closes(){
-    document.getElementById("all").style.display="none";
-    document.getElementById("myDiv").style.display = "block";
-}
